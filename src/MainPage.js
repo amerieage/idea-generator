ef9d9228df8cdc7235a3f3c2f5e1b95695d2e15d
Amerie Age <amerie@ronas-mbp.lan>
2020-04-04 18:40:25 -0500

    lots of links

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,29 +1,78 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import Space from "./Space"
 import Button from "@material-ui/core/Button"
+import ideas from "./ideas"
 
 const Root = styled.div`
   flex: 1;
   text-align: center;
 `
 
-const MainPage = () => (
-  <Root>
-    <Space direction="y" value="four" />
-    <Space direction="y" value="four" />
-    <Space direction="y" value="four" />
-    <em> Idea Generator </em>
-    <h1 style={{ marginTop: 0, fontSize: 50 }}>You Should....</h1>
-    <Space direction="y" value="four" />
-    <Space direction="y" value="four" />
-    <Button
-      variant="outlined"
-      style={{ color: "#5D484C", backgroundColor: "#FFC5CF" }}
-    >
-      Generate
-    </Button>
-  </Root>
-)
+const Idea = ({ idea }) => {
+  const { message, image, link } = idea
+
+  return (
+    <>
+      {link ? (
+        <a
+          style={{ margin: "16px 0px", display: "block" }}
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {message}
+        </a>
+      ) : (
+        <p>{message}</p>
+      )}
+      {image && image}
+    </>
+  )
+}
+
+const MainPage = () => {
+  const [currentIdea, setIdea] = useState(
+    ideas[Math.floor(Math.random() * ideas.length)]
+  )
+
+  const getNewIdea = () => {
+    let newIndex = Math.floor(Math.random() * ideas.length)
+
+    if (ideas[newIndex] === currentIdea) {
+      newIndex === ideas.length - 1
+        ? (newIndex = 0)
+        : (newIndex = ideas.length - 1)
+    }
+
+    setIdea(ideas[newIndex])
+  }
+
+  return (
+    <Root>
+      <Space direction="y" value="two" />
+
+      <em> Idea Generator </em>
+
+      <h1 style={{ marginTop: 0, fontSize: 50, marginBottom: 0 }}>
+        You Should....
+      </h1>
+
+      <Space direction="y" value="one" />
+
+      <Button
+        onClick={getNewIdea}
+        variant="outlined"
+        style={{ color: "#5D484C", backgroundColor: "#FFC5CF" }}
+      >
+        Generate another idea
+      </Button>
+
+      <Idea idea={currentIdea} />
+
+      <Space direction="y" value="four" />
+    </Root>
+  )
+}
 
 export default MainPage
---
+++ src/ideas.js
@@ -0,0 +1,179 @@
+import React from "react"
+
+const giphyHeight = 360
+
+export default [
+  {
+    message: "Read a book.",
+    link: "http://www.audible.com",
+  },
+  {
+    message: "Watch a funny movie.",
+    link: "https://www.netflix.com/",
+  },
+  {
+    message: "Show Bob Ross who's boss. Draw a picture. Only happy accidents.",
+    link: "https://sketch.io/sketchpad/",
+  },
+  {
+    message:
+      "Solve a puzzle. A rubix cube?! A word search!? The possibilities are endless!!!11!!1",
+    image: (
+      <>
+        <iframe
+          src="https://giphy.com/embed/rq7LNwzJGNY3u"
+          width="480"
+          height="270"
+          frameBorder="0"
+          class="giphy-embed"
+          title="Solve a Puzzle"
+          allowFullScreen
+        ></iframe>
+      </>
+    ),
+  },
+  {
+    message: "Learn a new language. Brag to your friends.",
+    link: "https://www.duolingo.com/",
+  },
+  {
+    message: "Cook something new.",
+    link: "https://www.allrecipes.com/",
+  },
+  {
+    message: "Listen to music.",
+    link: "https://www.spotify.com/us/",
+  },
+  {
+    message: "Take some pictures.",
+    link: "https://www.instagram.com/",
+  },
+  {
+    message: "Play a board game.",
+    link: "https://www.chess.com/",
+  },
+  {
+    message: "Become a paper folding wizard. Learn origami.",
+    link: "https://www.origamiway.com/",
+  },
+  {
+    message: "Clean your room and make your mother happy.",
+    image: (
+      <>
+        <iframe
+          src="https://giphy.com/embed/RjpE964WUAE5a"
+          height={giphyHeight}
+          frameBorder="0"
+          class="giphy-embed"
+          title="Clean Your Room"
+          allowFullScreen
+        ></iframe>
+      </>
+    ),
+  },
+  {
+    message: "Learn how to play an instrument.",
+    link: "https://yousician.com/",
+  },
+  {
+    message: "Take a trip back in time. Look at old photos or watch old shows.",
+    image: (
+      <>
+        <iframe
+          src="https://giphy.com/embed/wR4bJk4jF5Tl6"
+          height={giphyHeight}
+          frameBorder="0"
+          class="giphy-embed"
+          title="Take a Trip Back In Time"
+          allowFullScreen
+        ></iframe>
+      </>
+    ),
+  },
+  {
+    message: "Write a short story.",
+    link: "https://self-publishingschool.com/fiction-creative-writing-prompts/",
+  },
+  {
+    message: "Take a nap.",
+    image: (
+      <>
+        <iframe
+          src="https://giphy.com/embed/WA5GAr6XkMFNu"
+          width="480"
+          height="246"
+          frameBorder="0"
+          class="giphy-embed"
+          title="Take A Nap"
+          allowFullScreen
+        ></iframe>
+      </>
+    ),
+  },
+  {
+    message:
+      "Start a Kahoot server. Make a fun quiz, share the code with friends.",
+    link: "https://kahoot.com/",
+  },
+  {
+    message: "Hang out with your pets.",
+    image: (
+      <>
+        <iframe
+          src="https://giphy.com/embed/UiOU0PLf6jjTW"
+          height={giphyHeight}
+          frameBorder="0"
+          class="giphy-embed"
+          title="Hang Out With Your Pets"
+          allowFullScreen
+        ></iframe>
+      </>
+    ),
+  },
+  {
+    message: "Exercise!",
+    image: (
+      <>
+        <iframe
+          src="https://giphy.com/embed/1IBUZ2KEJBqXC"
+          height={giphyHeight}
+          frameBorder="0"
+          class="giphy-embed"
+          title="Exercise"
+          allowFullScreen
+        ></iframe>
+      </>
+    ),
+  },
+  {
+    message: "Call a friend",
+    image: (
+      <>
+        <iframe
+          src="https://giphy.com/embed/2XskdWMBUY1yxD6Hcic"
+          height={giphyHeight}
+          frameBorder="0"
+          class="giphy-embed"
+          title="Call a Friend"
+          allowFullScreen
+        ></iframe>
+      </>
+    ),
+  },
+  {
+    message: "Get some fresh air.",
+    image: (
+      <>
+        <iframe
+          src="https://giphy.com/embed/NYffbLkPpn08o"
+          width="480"
+          height="287"
+          frameBorder="0"
+          class="giphy-embed"
+          title="Get Some Fresh Air"
+          allowFullScreen
+        ></iframe>
+      </>
+    ),
+  },
+]
